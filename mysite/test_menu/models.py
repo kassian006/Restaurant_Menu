1b5eddec87f4e76994daa164a886354340a56770
Ulanbek60 <uislambekuulu@gmail.com>
2025-02-17 21:02:53 +0600

    pull translation

--- mysite/test_menu/models.py
+++ mysite/test_menu/models.py
@@ -33,6 +33,7 @@ class ImageAboutUs(models.Model):
 
 
 class MainMenu(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=233)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
@@ -57,6 +58,7 @@ class Drinks(models.Model):
 
 
 class FullMainMenu(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=233)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
