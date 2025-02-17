bfa1a3c624d6792f65d83eb6728012cca1409d89
Ulanbek60 <uislambekuulu@gmail.com>
2025-02-17 20:51:49 +0600

    push all

--- mysite/test_menu/admin.py
+++ mysite/test_menu/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
 from .models import *
 
+class ExtrasInline(admin.TabularInline):
+    model = Extras
+    extra = 1
+
+
+class DrinksInline(admin.TabularInline):
+    model = Drinks
+    extra = 1
+
+
+class BestSellerImageInline(admin.TabularInline):
+    model = BestSellerImage
+    extra = 1
+
+
+class ImageAboutUsInline(admin.TabularInline):
+    model = ImageAboutUs
+    extra = 1
+
+
+
 admin.site.register(Category)
 admin.site.register(AboutUs)
 admin.site.register(FullMainMenu)
-admin.site.register(Extras)
 admin.site.register(BestSellers)
 admin.site.register(BestSellerImage)
 admin.site.register(MainMenu)
--- mysite/test_menu/models.py
+++ mysite/test_menu/models.py
@@ -6,6 +6,7 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+
 class AboutUs(models.Model):
     title = models.CharField(max_length=512)
     description = models.TextField()
@@ -13,30 +14,7 @@ class AboutUs(models.Model):
     def __str__(self):
         return f'{self.title}, {self.description}'
 
-
-class ImageAboutUs(models.Model):
-    about_us = models.ForeignKey(AboutUs, on_delete=models.CASCADE)
-    image_about_us = models.ImageField(upload_to='about_us_images/', null=True, blank=True)
-
-
-class Extras(models.Model):
-    title = models.CharField(max_length=233)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f'{self.title}, {self.price}'
-
-class FullMainMenu(models.Model):
-    extras_name = models.ForeignKey(Extras, on_delete=models.CASCADE)
-    title = models.CharField(max_length=233)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_menu = models.ImageField(upload_to='image_menu/', null=True, blank=True)
-
-    def __str__(self):
-        return f'{self.title} {self.price}'
-
-class  BestSellers(models.Model):
+class BestSellers(models.Model):
     title = models.CharField(max_length=320)
     description = models.TextField()
 
@@ -48,8 +26,10 @@ class BestSellerImage(models.Model):
     best_seller = models.ForeignKey(BestSellers, on_delete=models.CASCADE)
     best_seller_image = models.ImageField(upload_to='best_seller_image/', null=True, blank=True)
 
-    def __str__(self):
-        return f'{self.best_seller}-{self.best_seller_image}'
+
+class ImageAboutUs(models.Model):
+    about_us = models.ForeignKey(AboutUs, on_delete=models.CASCADE)
+    image_about_us = models.ImageField(upload_to='about_us_images/', null=True, blank=True)
 
 
 class MainMenu(models.Model):
@@ -61,3 +41,26 @@ class MainMenu(models.Model):
         return f'{self.title}-{self.description}-{self.price}'
 
 
+class Extras(models.Model):
+    title = models.CharField(max_length=233)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.title}, {self.price}'
+
+class Drinks(models.Model):
+    title = models.CharField(max_length=233)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.title}, {self.price}'
+
+
+class FullMainMenu(models.Model):
+    title = models.CharField(max_length=233)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_menu = models.ImageField(upload_to='image_menu/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} {self.price}'
